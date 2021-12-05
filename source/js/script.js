05e8e7fd8b6465edc6077d922e2fe2d7e5600b00
Dmitry <upssq@mail.ru>
2021-12-05 13:13:06 +0300

    Разметка и стили ПП

--- source/js/script.js
+++ source/js/script.js
@@ -5,6 +5,27 @@ var body = page.querySelector('.body');
 var headerButton = body.querySelector('.header__contacts-button');
 var modal = body.querySelector('.modal');
 var modalClose = modal.querySelector('.modal__close');
+var sectionsSite = body.querySelector('.footer__sections-site');
+var contacts = body.querySelector('.footer__contacts');
+
+var keys = {
+  escape: 'Escape',
+  esc: 'Escape',
+};
+
+function onToggleSections() {
+  sectionsSite.classList.toggle('footer__sections-site--active');
+  contacts.classList.remove('footer__contacts--active');
+}
+
+function onToggleContacts() {
+  contacts.classList.toggle('footer__contacts--active');
+  sectionsSite.classList.remove('footer__sections-site--active');
+}
+
+// function isEscEven(evt) {
+//   evt.key === keys.escape || evt.key === keys.esc
+// }
 
 function addModal() {
   modal.classList.add('modal--active');
@@ -18,3 +39,7 @@ function removeModal() {
 
 headerButton.addEventListener('click', addModal);
 modalClose.addEventListener('click', removeModal);
+modal.addEventListener('click', removeModal);
+sectionsSite.addEventListener('click', onToggleSections);
+contacts.addEventListener('click', onToggleContacts);
+// isEscEvent.addEventListener('click', removeModal);
